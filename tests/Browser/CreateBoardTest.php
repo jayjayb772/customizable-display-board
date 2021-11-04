e7bbbccd114f089afd4425fb45e1f4a0daab85ce
Jay Bender <jayjayb100@msn.com>
2021-11-04 11:15:34 -0500

    basic dusk tests

---
+++ tests/Browser/CreateBoardTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class CreateBoardTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->markTestIncomplete();
+    }
+}
